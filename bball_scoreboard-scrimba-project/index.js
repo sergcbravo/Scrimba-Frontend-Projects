56be6909eb3b370723c106cc86cd4ad16f102201
sergcbravo <sergiocbravo1@gmail.com>
2024-02-29 17:42:33 -0600

    Added a 24 second shot-clock feature along with a div disguised as a button, used javascript to implement the timing feature

--- bball_scoreboard-scrimba-project/index.js
+++ bball_scoreboard-scrimba-project/index.js
@@ -1,5 +1,7 @@
 let homePoints = 0;
 let awayPoints = 0;
+let shotClockIsRunning = false;
+let shotClockTime = 24;
 
 document.getElementById('home-points-text').textContent = homePoints;
 document.getElementById('away-points-text').textContent = awayPoints;
@@ -32,4 +34,24 @@ function add2PointsAway() {
 function add3PointsAway() {
   awayPoints += 3;
   document.getElementById('away-points-text').textContent = awayPoints;
+}
+
+function shotClockStart() {
+  if(!shotClockIsRunning) {
+    shotClockTime = true;
+    shotClockTime = 24; // Reset to start time
+    document.getElementById('shot-clock-number').textContent = shotClockTime; // Initial display update
+
+    let timerInterval = setInterval(() => {
+      shotClockTime -=1; // Decrement time
+      document.getElementById('shot-clock-number').textContent = shotClockTime; // Update display
+
+      if (shotClockTime <= 0) {
+        clearInterval(timerInterval); // Stop the countdown
+        shotClockIsRunning = false;
+        // Change the color to red
+        document.getElementById('shot-clock-number').style.color = 'red';
+      }
+    }, 1000); // Update every second
+  }
 }
